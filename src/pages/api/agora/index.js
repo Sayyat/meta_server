9f7448daccba4bc32f0e94800a32ac9f390d581a
Sayyat <sayatraykulov@gmail.com>
2023-02-25 15:05:28 +0600

    agora settings changed

--- src/pages/api/agora/index.js
+++ src/pages/api/agora/index.js
@@ -1,14 +1,13 @@
 import {
-  RtcTokenBuilder,
-  RtmTokenBuilder,
-  RtcRole,
-  RtmRole,
+    RtcTokenBuilder,
+    RtmTokenBuilder,
+    RtcRole,
+    RtmRole,
 } from "agora-access-token";
+
 const appId = "b79fdafba2a84c2a9e28e9b250ff90df";
 const appCertificate = "20ce945d635643fabc5a2be06f84d065";
-const channelName = "Alem";
-const uid = 0;
-const userAccount = "User account";
+const defaultChannel = "Alem";
 const role = RtcRole.PUBLISHER;
 const expirationTimeInSeconds = 24 * 60 * 60;
 const currentTimestamp = Math.floor(Date.now() / 1000);
@@ -16,18 +15,17 @@ const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
 // Build token with uid
 
 export default function handler(req, res) {
-  if (req.method == "POST") {
-    const { channel, uid } = JSON.parse(req.body);
+    const {channel, uid} = req.body
     const tokenA = RtcTokenBuilder.buildTokenWithUid(
-      appId,
-      appCertificate,
-      channel,
-      uid,
-      role,
-      privilegeExpiredTs
+        appId,
+        appCertificate,
+        channel? channel : defaultChannel,
+        uid?uid:0,
+        role,
+        privilegeExpiredTs
     );
-    res.status(200).json({ token: tokenA });
-  }
+
+    res.status(200).json({token: tokenA});
 }
 
 
